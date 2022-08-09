4f7e7b56f7a1e01435debf1d848c135ee5c0772f
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-09 18:24:35 -0300

    Adicionando o Nodemon na aplicação

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -5,7 +5,7 @@
 
  app.get('/', (request, response) => {
     //return response.send("Hello world")
-     return response.json({message: "Hello world ignite!"})
+     return response.json({message: "Hello world ignite! - Fundamentos NodeJS!"})
  });
 
  app.listen(3333)
